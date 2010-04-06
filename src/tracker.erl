b539c40832487557db54ecf6944b0eb72113ea92
Astro <astro@spaceboyz.net>
2010-04-07 01:18:20 +0200

    tracker: ipv6 support

--- src/tracker.erl
+++ src/tracker.erl
@@ -8,22 +8,42 @@
 peer_list_from_info(InfoDict) ->
     Peers4 =
 	case lists:keysearch(<<"peers">>, 1, InfoDict) of
-	    {value, {_, Peers}} when is_list(Peers) ->
+	    {value, {_, P4}} when is_list(P4) ->
 		lists:map(
 		  fun(Peer) ->
 			  {value, {"ip", Host, _}} = keysearch("ip", 1, Peer),
 			  {ok, IP} = inet_parse:address(Host),
 			  {value, {"port", Port, _}} = keysearch("port", 1, Peer),
 			  {IP, Port}
-		  end, Peers);
-	    {value, {_, Peers}} when is_binary(Peers) ->
-		peer_list_from_compact(Peers)
-	end.
+		  end, P4);
+	    {value, {_, P4}} when is_binary(P4) ->
+		peer_list_from_compact4(P4);
+	    _ ->
+		[]
+	end,
+    Peers6 =
+	case lists:keysearch(<<"peers6">>, 1, InfoDict) of
+	    {value, {_, P6}} when is_binary(P6) ->
+		peer_list_from_compact6(P6);
+	    _ ->
+		[]
+	end,
+    Peers4 ++ Peers6.
 
-peer_list_from_compact(<<H1:8, H2:8, H3:8, H4:8, Port:16/big, Peers/binary>>) ->
+peer_list_from_compact4(<<H1:8, H2:8, H3:8, H4:8, Port:16/big,
+			  Peers/binary>>) ->
     Host = {H1, H2, H3, H4},
-    [{Host, Port} | peer_list_from_compact(Peers)];
-peer_list_from_compact(<<>>) ->
+    [{Host, Port} | peer_list_from_compact4(Peers)];
+peer_list_from_compact4(<<>>) ->
+    [].
+
+peer_list_from_compact6(<<H1:16/big, H2:16/big, H3:16/big, H4:16/big,
+			  H5:16/big, H6:16/big, H7:16/big, H8:16/big,
+			  Port:16/big,
+			  Peers/binary>>) ->
+    Host = {H1, H2, H3, H4, H5, H6, H7, H8},
+    [{Host, Port} | peer_list_from_compact6(Peers)];
+peer_list_from_compact6(<<>>) ->
     [].
 
 % This is one of the announcements done at regular intervals.
@@ -79,7 +99,7 @@ url_escape(R, [C | S]) when C >= $A, C =< $Z ->
 url_escape(R, [C | S]) when C >= $a, C =< $z ->
     url_escape(R ++ [C], S);
 url_escape(R, [C | S]) ->
-    {ok, Escaped, _} = regexp:gsub(flatten(io_lib:format("%~2.16B", [C])), " ", "0"),
+    {ok, Escaped, _} = flatten(io_lib:format("%~2.16.0B", [C])),
     url_escape(R ++ Escaped, S);
 url_escape(R, Number) when is_integer(Number) ->
     R ++ io_lib:format("~B", [Number]);
