a6ed26a68a62e047521a1cdb172af6fb0b0d6c1e
Astro <astro@spaceboyz.net>
2010-04-21 02:34:33 +0200

    start.sh: env by param

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+ENV=""
+[ "$1" ] && ENV="-servtorrent seedlist \"$1\""
+[ "$2" ] && ENV="$ENV -servtorrent wire_port $2"
+
 erl -make && \
 erl +K true -smp auto \
     -pa ebin \
-    -s servtorrent
+    -s servtorrent \
+    $ENV
