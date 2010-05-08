e5baf718101013aba52bdbdc1ad1f9b7139d2aa1
Astro <astro@spaceboyz.net>
2010-05-09 00:25:14 +0200

    seedlist: allow including other seedlists

--- src/seedlist.erl
+++ src/seedlist.erl
@@ -10,63 +10,90 @@ start_link() ->
     {ok, Filename} = application:get_env(servtorrent, seedlist),
     Pid = spawn_link(
 	    fun() ->
-		    loop(Filename, nil)
+		    loop(Filename, [])
 	    end),
     {ok, Pid}.
 
 -define(RELOAD_INTERVAL, 10).
-loop(Filename, ModTime1) ->
-    ModTime2 = get_mtime(Filename),
+loop(Filename, ModTimes) ->
+    Modified = case ModTimes of
+		   [] -> true;
+		   _ ->
+		       lists:foldl(
+			 fun({Path, Time}, Modified) ->
+				 CurrentTime = get_mtime(Path),
+				 if
+				     CurrentTime > Time ->
+					 logger:log(control, info,
+						    "~s has been modified, mtime=~B", [Path, CurrentTime]),
+					 true;
+				     true ->
+					 Modified
+				 end
+			 end, false, ModTimes)
+	       end,
     if
-	ModTime2 =/= ModTime1 ->
-	    io:format("~s has been modified~n", [Filename]),
+	Modified ->
 	    case (catch load_seedlist(Filename)) of
 		{'EXIT', Reason} ->
-		    io:format("Cannot load seedlist ~s: ~p~n",
-			      [Filename, Reason]);
-		SeedList ->
-		    torrentdb:apply_seedlist(SeedList)
+		    logger:log(control, fatal,
+			       "Cannot load seedlist ~s: ~p~n",
+			       [Filename, Reason]);
+		{ok, SeedList, NewModTimes} ->
+		    logger:log(control, info,
+			       "Loaded ~B seeds by ~s",
+			       [length(SeedList), Filename]),
+		    torrentdb:apply_seedlist(SeedList),
+		    ?MODULE:loop(Filename, NewModTimes)
 	    end;
 	true ->
-	    not_modified
-    end,
-
-    receive
-    after ?RELOAD_INTERVAL * 1000 ->
-	    ?MODULE:loop(Filename, ModTime2)
+	    receive
+	    after ?RELOAD_INTERVAL * 1000 ->
+		    ?MODULE:loop(Filename, ModTimes)
+	    end
     end.
 
+
 load_seedlist(Filename) ->
+    ModTime = {Filename, get_mtime(Filename)},
     File = backend:read_file(Filename),
     {SeedsEl, _} = xmerl_scan:string(binary_to_list(File)),
     #xmlElement{content = SeedsChildren} = SeedsEl,
-    lists:filter(
-      fun({_, _}) -> true;
-	 (_) -> false
-      end,
-      lists:map(fun(#xmlElement{name = seed} = SeedEl) ->
-			try {get_el_text(torrent, SeedEl),
-			     get_el_text(data, SeedEl)}
-			catch _ ->
-				nil
-			end;
-		   (_) ->
-			nil
-		end, SeedsChildren)).
+    lists:foldl(
+      fun(#xmlElement{name = seed} = SeedEl,
+	  {ok, SeedList, ModTimes} = R) ->
+	      try {ok,
+		   [{get_el_text(torrent, SeedEl),
+		     get_el_text(data, SeedEl)} | SeedList],
+		   ModTimes}
+	      catch _ ->
+		      R
+	      end;
+	 (#xmlElement{name = include} = IncludeEl,
+	  {ok, SeedList, ModTimes}) ->
+	      Filename1 = get_el_text(IncludeEl),
+	      {ok, SeedList1, ModTimes1} = load_seedlist(Filename1),
+	      {ok, SeedList1 ++ SeedList, ModTimes1 ++ ModTimes};
+	 (_, Result) ->
+	      Result
+      end, {ok, [], [ModTime]}, SeedsChildren).
 
-get_el_text(Name, #xmlElement{content = Children}) ->
-    get_el_text1(Name, Children).
-get_el_text1(Name, []) ->
-    exit({not_found, Name});
-get_el_text1(Name, [#xmlElement{name = Name,
-				content = Children} | _]) ->
+
+get_el_text(#xmlElement{content = Children}) ->
     lists:append(
       lists:map(
 	fun(#xmlText{value = Text}) ->
 		Text;
 	   (_) ->
 		[]
-	end, Children));
+	end, Children)).
+
+get_el_text(Name, #xmlElement{content = Children}) ->
+    get_el_text1(Name, Children).
+get_el_text1(Name, []) ->
+    exit({not_found, Name});
+get_el_text1(Name, [#xmlElement{name = Name} = El | _]) ->
+    get_el_text(El);
 get_el_text1(Name, [_ | Els]) ->
     get_el_text1(Name, Els).
 
