f89ebb45dc1501126815e3db4ca389d388cafc34
Hans Ulrich Niedermann <hun@n-dimensional.de>
2011-02-05 17:07:36 +0100

    Replace list_to_binary([]) by direct <<>> construct
    Why construct a list and then convert it to a binary,
    if we can construct a binary in the first place?

--- src/benc.erl
+++ src/benc.erl
@@ -85,20 +85,20 @@ parse_str(Len, Str, <<C, Remaining/binary>>) ->
 to_binary([{_, _} | _] = Dict) ->
     Elements = [[to_binary(K), to_binary(V)] || {K, V} <- Dict],
     Bin = list_to_binary(Elements),
-    list_to_binary(["d", Bin, "e"]);
+    <<"d", Bin/binary, "e">>;
 
 to_binary(List) when is_list(List) ->
     Elements = [to_binary(E) || E <- List],
     Bin = list_to_binary(Elements),
-    list_to_binary(["l", Bin, "e"]);
+    <<"l", Bin/binary, "e">>;
 
 to_binary(I) when is_integer(I) ->
     Bin = list_to_binary(integer_to_list(I)),
-    list_to_binary(["i", Bin, "e"]);
+    <<"i", Bin/binary, "e">>;
 
 to_binary(<<String/binary>>) ->
     Length = list_to_binary(integer_to_list(size(String))),
-    list_to_binary([Length, $:, String]).
+    <<Length/binary, ":", String/binary>>.
 
      
 
