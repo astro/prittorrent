58204c8d180e70bd2decd73cf7dc47f23702cd6f
Hans Ulrich Niedermann <hun@n-dimensional.de>
2011-02-05 14:13:29 +0100

    Make default build run without behaviour warning

--- Emakefile
+++ Emakefile
@@ -1,1 +1,4 @@
+%% Compile behaviours first
+{'src/backend',[{outdir, "ebin"}]}.
+%% Compile the remaining source code
 {'src/*',[{outdir, "ebin"}]}.
--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ ENV=""
 [ "$1" ] && ENV="-servtorrent seedlist \"$1\""
 [ "$2" ] && ENV="$ENV -servtorrent wire_port $2"
 
-erl -make && \
+erl -pa ebin -make && \
 erl +K true -smp auto \
     -pa ebin \
     -noshell \
