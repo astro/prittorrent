62e12b052f9666dd513a75cf8007a4a59e9e0971
Astro <astro@spaceboyz.net>
2012-04-16 02:32:10 +0200

    hasher_sup has 2 hasher_workers

--- apps/hasher/src/hasher_sup.erl
+++ apps/hasher/src/hasher_sup.erl
@@ -24,5 +24,9 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    NWorkers = 2,
+    Workers = [{{worker, N}, {hasher_worker, start_link, []},
+		permanent, 5000, worker, [hasher_worker, hasher_hash]}
+	       || N <- lists:seq(1, NWorkers)],
+    {ok, { {one_for_one, 5, 10}, Workers} }.
 
