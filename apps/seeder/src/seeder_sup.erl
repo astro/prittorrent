06b13aab0daf4608ed1d079cd25b1cb848a4eefa
Astro <astro@spaceboyz.net>
2012-04-29 00:36:35 +0200

    rm old servtorrent, add seeder app

---
+++ apps/seeder/src/seeder_sup.erl
@@ -0,0 +1,31 @@
+
+-module(seeder_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    SeederListener =
+	{seeder_listener, {seeder_listener, start_link, []},
+	 permanent, 2000, worker, [seeder_listener]},
+    {ok, { {one_for_one, 5, 10}, [SeederListener]} }.
+
