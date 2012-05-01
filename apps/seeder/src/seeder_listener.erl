e55db1462e98c7a5b6b0cbe799d16aa73ea05564
Astro <astro@spaceboyz.net>
2012-05-01 04:13:54 +0200

    seeder_listener: listen on $BIND_IP:6881 if set

--- apps/seeder/src/seeder_listener.erl
+++ apps/seeder/src/seeder_listener.erl
@@ -3,9 +3,16 @@
 -export([start_link/0, stop/0]).
 
 start_link() ->
+    IP = case os:getenv("BIND_IP") of
+	     false ->
+		 {0, 0, 0, 0, 0, 0, 0, 0};
+	     IP1 ->
+		 {ok, IP2} = inet_parse:address(IP1),
+		 IP2, 80
+	 end,
     cowboy:start_listener(
       seeder_wire_listener, 32,
-      cowboy_tcp_transport, [{ip, {0, 0, 0, 0, 0, 0, 0, 0}},
+      cowboy_tcp_transport, [{ip, IP},
 			     {port, 6881},
 			     {max_connections, 768},
 			     %% Not so many but long-lived connections
