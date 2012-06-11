5ae710badeaf754d06067a804ca8ea08f6234206
Astro <astro@spaceboyz.net>
2012-06-11 05:09:27 +0200

    ui_template:explain_error1/2: {xml_parser, _, _, _}

--- apps/ui/src/ui_template.erl
+++ apps/ui/src/ui_template.erl
@@ -1392,13 +1392,20 @@ explain_error1(feed, {http, Status}) when is_integer(Status) ->
     io_lib:format("HTTP ~B, expected: 200 Ok", [Status]);
 explain_error1(enclosure, {http, Status}) when is_integer(Status) ->
     io_lib:format("HTTP ~B, expected: 206 Partial Content", [Status]);
+explain_error1(_, invalid_feed) ->
+    <<"Invalid feed">>;
 explain_error1(_, no_content_length) ->
     <<"Web server didn't include Content-Length on HEAD">>;
 explain_error1(_, timeout) ->
     <<"Timeout contacting server">>;
 explain_error1(_, connection_closed) ->
     <<"Connection closed">>;
-explain_error1(_, Term) ->
+explain_error1(_, {xml_parser, parsing, parsing_failed, {_, Message}})
+  when is_list(Message) ->
+    list_to_binary([<<"XML parser error: ">>, Message]);
+explain_error1(_, {xml_parser, _, _, _}) ->
+    <<"XML parser error">>;
+explain_error1(_Scope, _Term) ->
     <<"Internal error">>.
 
 
