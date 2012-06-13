142023980d135a90d4a26254af9a6d0c3e2e1acb
Astro <astro@spaceboyz.net>
2012-06-13 23:50:50 +0200

    podpress widget: refine button image, fix layout

--- apps/ui/src/ui_widget.erl
+++ apps/ui/src/ui_widget.erl
@@ -135,10 +135,10 @@ $(document).ready(function() {
 	    console.log(\"resolve\",url,info);
 	    var torrent = info && info.sources && info.sources[0] && info.sources[0].torrent;
 	    if (info && torrent) {
-		var t = $('<div class=\"podPress_downloadlinks\"><a class=\"podpress_downloadimglink\" title=\"Download via BitTorrent\" type=\"application/x-bittorrent\"><img src=\"http://bitlove.org/static/bitlove-button.png\" class=\"podPress_imgicon\"></a><span class=\"podpress_mediafile_title\"><span class=\"s\"></span><span class=\"l\"></span><span class=\"d\"></span></span></div>');
+		var t = $('<div class=\"podPress_downloadlinks\"><a class=\"podpress_downloadimglink\" title=\"Download via BitTorrent\" type=\"application/x-bittorrent\"><img src=\"http://bitlove.org/static/bitlove-button.png\" class=\"podPress_imgicon\"></a><span class=\"podpress_mediafile_title\"> <span class=\"s\"></span><span class=\"l\"></span><span class=\"d\"></span></span></div>');
 		t.find('a').attr('href', torrent);
-		t.find('.s').text(info.seeders + \" Seeders, \");
-		t.find('.l').text(info.leechers + \" Leechers\");
+		t.find('.s').text(info.seeders + (info.seeders == 1 ? \"Seeder, \" : \" Seeders, \"));
+		t.find('.l').text(info.leechers + (info.leechers == 1 ? \"Leecher\" : \" Leechers\"));
 		if (info.downloaded == 1) {
 		    t.find('.d').text(\", 1 Download\");
 		} else if (info.downloaded > 1) {
